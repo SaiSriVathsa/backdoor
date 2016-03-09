01c73f21f7f16862743df64712e3a018e9f08a85
SaiSriVathsa <srivathsaeric@gmail.com>
2016-03-10 01:51:57 +0530

    ready for presentation

--- sample.php
+++ sample.php
@@ -4,20 +4,27 @@ mysql_select_db("sample") ;
 	if(! $con ) {
       die('Could not connect: ' . mysql_error());
   	}
-   	if(isset($_POST['submit']))
+  	else
+  	{
+	if(isset($_POST['submit']))
    	{
    		$username=$_POST['user'];
    		$password=$_POST['password'];
    		$sql="select * from users where name='$username'";
-   		if ($query=mysql_query($sql,$con) or die(mysql_error()))
-   		{
-   			while($row=mysql_fetch_array($query) or die(mysql_error()))
-	   			{
-	   			echo $row['flag']."<br>";
-   				}
-   		}
+   		if (!$_POST['user'] | !$_POST['password'])
+   			echo "login unsuccesful";
    		else
-   			echo "problem with query";
-   	}
-   
+   			{
+   				if ($query=mysql_query($sql,$con) or die(mysql_error()))
+   					{
+   						while($row=mysql_fetch_array($query) or die(mysql_error()))
+	   						{
+	   							echo $row['flag']."<br>";
+   							}
+   					}
+   				else
+   					echo "problem with query";
+   	    	}
+    	}
+    }
 ?>
