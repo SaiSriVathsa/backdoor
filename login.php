3088fc9686403f51a44edd1310b46fc49d19b661
SaiSriVathsa <srivathsaeric@gmail.com>
2016-03-09 13:07:06 +0530

    updating record succesful

--- login.php
+++ login.php
@@ -1,6 +1,13 @@
 <?php
 $con=mysql_connect("localhost","root","hello");
 $db_selected = mysql_select_db('sample', $con);
+$servername = "localhost";
+$username = "root";
+$password = "hello";
+$dbname = "sample";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 if (isset($_POST['submit']))
 {
 	$username=$_POST['user'];
@@ -22,10 +29,10 @@ if (isset($_POST['submit']))
 	};
 	$sql = "UPDATE users SET rand='$username' WHERE id=1";
 
-if (mysqli_query($con, $sql)) {
+if (mysqli_query($conn, $sql)) {
     echo "Record updated successfully";
 } else {
-    echo "Error updating records: " . mysqli_error($con);
+    echo "Error updating records: " . mysqli_error($conn);
 }
 
 	}	
