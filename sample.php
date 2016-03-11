0b0045332dd8d9d49711a4afc41ef4b25e418278
SaiSriVathsa <srivathsaeric@gmail.com>
2016-03-11 17:43:57 +0530

    first submission

--- sample.php
+++ sample.php
@@ -10,20 +10,19 @@ mysql_select_db("sample") ;
    	{
    		$username=$_POST['user'];
    		$password=$_POST['password'];
-   		$sql="select * from users where name='$username'";
+   		$username=str_replace("=","=\ ",$username);
+      $sql="select * from users where name='$username'";
    		if (!$_POST['user'] | !$_POST['password'])
    			echo "login unsuccesful";
    		else
    			{
-   				if ($query=mysql_query($sql,$con) or die(mysql_error()))
+   				if ($query=mysql_query($sql,$con) )
    					{
    						while($row=mysql_fetch_array($query) or die(mysql_error()))
 	   						{
 	   							echo $row['flag']."<br>";
    							}
    					}
-   				else
-   					echo "problem with query";
    	    	}
     	}
     }
